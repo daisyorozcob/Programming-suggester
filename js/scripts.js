edb7a7dde786e4a2d7ccc1cb9f4ccbfa31f126fb
Daisy Orozco <daisyyy.ob@gmail.com>
2023-10-20 10:01:53 -0700

    add coto js

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("survey-form");
+const submitButton = document.getElementById("submit-button");
+const resultContainer = document.getElementById("result");
+const suggestion = document.getElementById("language-suggestion");
