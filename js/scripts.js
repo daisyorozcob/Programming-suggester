0ebf66a89b83ba658c1e0eb7154c51d44d46e8dc
Daisy Orozco <daisyyy.ob@gmail.com>
2023-10-20 11:16:27 -0700

    add event listener for button

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ const form = document.getElementById("survey-form");
 const submitButton = document.getElementById("submit-button");
 const resultContainer = document.getElementById("result");
 const suggestion = document.getElementById("language-suggestion");
+
+submitButton.addEventListener("click", () => {
+    const answer1 = document.getElementById("question1").value;
+    const answer2 = document.getElementById("question2").value;
+    const answer3 = document.getElementById("question3").value;
+    const answer4 = document.getElementById("question4").value;
+    const answer5 = document.getElementById("question5").value}
+    
+    )
