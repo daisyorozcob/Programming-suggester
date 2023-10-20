62f8457e0ef6ea5ddc3b0697bf3c655ff28124a7
Daisy Orozco <daisyyy.ob@gmail.com>
2023-10-20 13:33:31 -0700

    add style

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,14 @@ const resultContainer = document.getElementById("result");
 const suggestion = document.getElementById("language-suggestion");
 
 submitButton.addEventListener("click", () => {
-    const answer1 = document.getElementById("question1").value;
-    const answer2 = document.getElementById("question2").value;
-    const answer3 = document.getElementById("question3").value;
-    const answer4 = document.getElementById("question4").value;
-    const answer5 = document.getElementById("question5").value}
-    
-    let suggestedLanguage = "";
+  const answer1 = document.getElementById("question1").value;
+  const answer2 = document.getElementById("question2").value;
+  const answer3 = document.getElementById("question3").value;
+  const answer4 = document.getElementById("question4").value;
+  const answer5 = document.getElementById("question5").value;
+
+
+  let suggestedLanguage = "";
 
   if (answer1 === "pancakes") {
     suggestedLanguage = "Python";
@@ -21,4 +22,13 @@ submitButton.addEventListener("click", () => {
   } else if (answer4 === "dark") {
     suggestedLanguage = "C#";
   } else if (answer5 === "cats") {
-    )
+    suggestedLanguage = "Swift";
+  } else {
+    suggestedLanguage = "Java";
+  }
+
+  suggestion.textContent = suggestedLanguage;
+  resultContainer.classList.remove("hidden");
+
+  form.reset();
+});
